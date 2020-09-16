3d61a3d35bf19f5a51e102060c40343fd9edfc3b
Bruno Pastre <pastre68@gmail.com>
2020-09-15 23:20:34 -0300

    Loads scene

--- Visu/ViewController.swift
+++ Visu/ViewController.swift
@@ -6,15 +6,55 @@
 //  Copyright © 2020 Bruno Pastre. All rights reserved.
 //
 
+import SpriteKit
 import UIKit
 
-class ViewController: UIViewController {
+class VSScene: SKScene {
+    
+}
 
+class VSSceneManager: NSObject, SKSceneDelegate {
+    
+}
+
+class ViewController: UIViewController, SKSceneDelegate {
+
+    let scene: VSScene = {
+        let scene = VSScene()
+        
+        scene.backgroundColor = .blue
+        
+        return scene
+    }()
+    
+    let skView: SKView = {
+        let view = SKView()
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .gray
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.setupSKView()
+        self.setupVSScene()
+    }
+    
+    func setupSKView() {
+        
+        self.view.addSubview(self.skView)
+        
+        skView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        skView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        skView.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
+        skView.heightAnchor.constraint(equalTo: self.view.heightAnchor).isActive = true
     }
 
+    func setupVSScene() {
+        self.skView.presentScene(self.scene)
+    }
 
 }
 
